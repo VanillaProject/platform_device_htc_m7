ba224df9706c00e6f40a5ac91e9a336a39dc712a
DragunKorr <vanillaaosp@gmail.com>
2013-06-07 13:06:08 -0400

    Update Device to Vanilla m7

--- full_m7tmo.mk
+++ full_m7tmo.mk
@@ -14,7 +14,7 @@ $(call inherit-product, device/htc/m7/device_m7.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m7 BUILD_ID=JZO54K BUILD_FINGERPRINT="tmous/m7/m7:4.1.2/JZO54K/170484.7:user/release-keys" PRIVATE_BUILD_DESC="1.27.531.7 CL170484 release-keys"
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := m7
+PRODUCT_NAME := vanilla_m7
 PRODUCT_DEVICE := m7
 PRODUCT_BRAND := htc
 PRODUCT_MANUFACTURER := htc
